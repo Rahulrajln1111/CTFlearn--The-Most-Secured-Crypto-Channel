22499df57ffcb1bf51bb16b9861c54fb941346c2
Rahul Razz <83722484+Rahulrajln1111@users.noreply.github.com>
2024-03-12 08:43:15 -0400

    Update bb84.py

--- bb84.py
+++ bb84.py
@@ -1,3 +1,5 @@
+import math
+import base64
 with open("transmission1.txt","rb")  as file1,open("transmission2.txt","rb") as file2:
 	f1 = file1.read()
 	f2 = file2.read()
@@ -7,9 +9,10 @@ with open("transmission1.txt","rb")  as file1,open("transmission2.txt","rb") as 
 			val+='1'
 		if chr(a) == '/' and chr(b) == 'x' or chr(a) == '-' and chr(b) == '+':
 			val+='0'
-print(val)
 
-#After getting binary values ,decode it with cyberChef to get flag
-#Flag{Qu4n7umCryp70gr4phyIs4Fu7ur3}
+integ = int(val,2) # decimal value from binary
+
+print(base64.b64decode(bytes.fromhex(hex(integ)[2:]).decode()[4:]).decode()) #from decimal to hex string, then  hex to char string(which was base64 encoded)
+
 
 
